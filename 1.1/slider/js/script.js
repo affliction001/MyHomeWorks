46b23aa68c077d94e3f3fee1ce8266f9d1019c8d
affliction001 <affliction001@mail.ru>
2018-06-23 13:33:14 +0300

    hw

--- 1.1/slider/js/script.js
+++ 1.1/slider/js/script.js
@@ -1,13 +1,15 @@
 'use strict';
 
-let images = ['i/airmax.png',
-              'i/airmax-jump.png',
-              'i/airmax-on-foot.png',
-              'i/airmax-playground.png',
-              'i/airmax-top-view.png'];
+let images = ['./i/airmax.png',
+              './i/airmax-jump.png',
+              './i/airmax-on-foot.png',
+              './i/airmax-playground.png',
+              './i/airmax-top-view.png'];
 
 let slider = document.getElementById('slider');
 let counter = 0;
+
+slider.src = images[4];
 
 setInterval(() => {
   slider.src = images[counter];
