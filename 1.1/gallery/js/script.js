bb54a082c9c44f45021a37c75f88d65aa1a876af
affliction001 <affliction001@mail.ru>
2018-06-23 13:54:48 +0300

    hw

---
+++ 1.1/gallery/js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let images = ['./i/breuer-building.jpg',
+              './i/guggenheim-museum.jpg',
+              './i/headquarters.jpg',
+              './i/IAC.jpg',
+              './i/new-museum.jpg'];
+
+let prev = document.getElementById('prevPhoto');
+let next = document.getElementById('nextPhoto');
+
+let photo = document.getElementById('currentPhoto');
+photo.src = images[0];
+
+let counter = 0;
+
+function handlerPrev() {
+  counter === 0 ? counter = 4 : counter--;
+  photo.src = images[counter];
+}
+
+function handlerNext() {
+  counter === 4 ? counter = 0 : counter++;
+  photo.src = images[counter];
+}
+
+prev.onclick = handlerPrev;
+next.onclick = handlerNext;
