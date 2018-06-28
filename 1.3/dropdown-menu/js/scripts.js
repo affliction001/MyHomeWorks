6dfc5b931bd94f99564ca46f96b0d7d1fec51c77
Vladislav Semenov <affliction001@mail.ru>
2018-06-28 20:32:13 +0300

    corrected

--- 1.3/dropdown-menu/js/scripts.js
+++ 1.3/dropdown-menu/js/scripts.js
@@ -3,7 +3,5 @@
 const dropdownMenu = document.getElementsByClassName('wrapper-dropdown');
 
 dropdownMenu[0].onclick = () => {
-  dropdownMenu[0].classList.contains('active') ?
-    dropdownMenu[0].classList.remove('active') :
-    dropdownMenu[0].classList.add('active');
+  dropdownMenu[0].classList.toggle('active');
 }
