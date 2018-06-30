4c6924111193bcaba133438de356e8897b7dbca1
Vladislav Semenov <affliction001@mail.ru>
2018-06-30 12:50:32 +0300

    For checking.

---
+++ 2.1/easter-egg/js/scripts.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let unlockStatus = 0;
+
+function showNavBar(event) {
+  if (!event.ctrlKey) {
+    return;
+  }
+
+  if (!event.altKey) {
+    return;
+  }
+
+  if (event.code === 'KeyT') {
+    document.getElementsByTagName('nav')[0].classList.toggle('visible');
+  }
+}
+
+function easterEgg(event) {
+  const secretCode = ['KeyY','KeyT','KeyN','KeyJ','KeyK','KeyJ','KeyU','KeyB','KeyZ'];
+
+  if (event.code === secretCode[unlockStatus]) {
+    unlockStatus++;
+
+    if (unlockStatus === 9) {
+      document.getElementsByClassName('secret')[0].classList.add('visible');
+    }
+  } else {
+    event.code === secretCode[0] ? unlockStatus = 1 : unlockStatus = 0;
+  }
+}
+
+document.addEventListener('keydown', showNavBar);
+document.addEventListener('keydown', easterEgg);
