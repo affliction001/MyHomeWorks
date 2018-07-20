eb0defa2c0a7d5ecf20a6728c3c45c8c0a1c2a96
Vladislav Semenov <affliction001@mail.ru>
2018-07-20 22:01:57 +0300

    For checking

--- 3.3/sign-in-form/js/auth.js
+++ 3.3/sign-in-form/js/auth.js
@@ -1,17 +1,15 @@
 'use strict';
 
-const formSignIn = new FormData(document.getElementsByClassName('sign-in-htm')[0]);
-const formSignUp = new FormData(document.getElementsByClassName('sign-up-htm')[0]);
-
 const buttonIn = document.querySelector('.sign-in-htm .button');
 
 buttonIn.addEventListener('click', function (e) {
   event.preventDefault();
+
   const xhr = new XMLHttpRequest();
-  xhr.addEventListener("load", onLoad);
   xhr.open('POST', 'https://neto-api.herokuapp.com/signin');
   xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify(formSignIn));
+  xhr.addEventListener('load', onLoad);
+  xhr.send(JSON.stringify(getSignInFields()));
 
   function onLoad() {
     const answer = JSON.parse(xhr.responseText);
@@ -28,11 +26,12 @@ const buttonUp = document.querySelector('.sign-up-htm .button');
 
 buttonUp.addEventListener('click', function (e) {
   event.preventDefault();
+
   const xhr = new XMLHttpRequest();
-  xhr.addEventListener("load", onLoad);
   xhr.open('POST', 'https://neto-api.herokuapp.com/signup');
   xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify(formSignUp));
+  xhr.addEventListener('load', onLoad);
+  xhr.send(JSON.stringify(getSignUpFields()));
 
   function onLoad() {
     const answer = JSON.parse(xhr.responseText);
@@ -44,3 +43,28 @@ buttonUp.addEventListener('click', function (e) {
     }
   }
 });
+
+function getSignInFields() {
+  const email = document.querySelector('.sign-in-htm #email');
+  const pass = document.querySelector('.sign-in-htm #pass');
+
+  let result = {};
+  result[email.name] = email.value;
+  result[pass.name] = pass.value;
+
+  return result;
+}
+
+function getSignUpFields() {
+  const email = document.querySelector('.sign-up-htm #email');
+  const pass = document.querySelectorAll('.sign-up-htm #pass');
+
+  let result = {};
+  result[email.name] = email.value;
+
+  Array.from(pass).forEach(p => {
+    result[p.name] = p.value;
+  });
+
+  return result;
+}
