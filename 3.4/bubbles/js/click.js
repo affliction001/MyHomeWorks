443f973bd3af39a8ca6367702ecb3bdcd86c132a
Vladislav Semenov <affliction001@mail.ru>
2018-07-22 23:39:48 +0300

    For checking

--- 3.4/bubbles/js/click.js
+++ 3.4/bubbles/js/click.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const canvas = document.querySelector("canvas");
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+connection.addEventListener('open', function(e) {
+  showBubbles(connection);
+});
+
+canvas.addEventListener('click', function(e) {
+  const clickXY = {'x': e.clientX, 'y': e.clientY};
+  connection.send(JSON.stringify(clickXY));
+});
