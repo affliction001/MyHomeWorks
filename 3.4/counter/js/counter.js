98099223914cf19a99e3e6aa284881e03c9bb803
Vladislav Semenov <affliction001@mail.ru>
2018-07-23 13:52:00 +0300

    For checking

--- 3.4/counter/js/counter.js
+++ 3.4/counter/js/counter.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const count = document.querySelector('.counter');
+const error = document.querySelector('output.errors');
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+connection.addEventListener('message', function(e) {
+  count.textContent = JSON.parse(e.data).connections;
+  error.textContent = JSON.parse(e.data).errors;
+});
+
+window.addEventListener('beforeunload', function(e) {
+  connection.close(1000, 'close');
+});
