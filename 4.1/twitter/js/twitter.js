99ec2336a126022e096df44238aa8a73309739c4
Vladislav Semenov <affliction001@mail.ru>
2018-07-24 18:38:53 +0300

    For checking

--- 4.1/twitter/js/twitter.js
+++ 4.1/twitter/js/twitter.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function callback(obj) {
+  document.querySelector('[data-wallpaper]').src = obj.wallpaper;
+  document.querySelector('[data-username]').textContent = obj.username;
+  document.querySelector('[data-description]').textContent = obj.description;
+  document.querySelector('[data-pic]').src = obj.pic;
+  document.querySelector('[data-tweets]').textContent = obj.tweets;
+  document.querySelector('[data-followers]').textContent = obj.followers;
+  document.querySelector('[data-following]').textContent = obj.following;
+}
+
+const scriptNode = document.createElement('script');
+scriptNode.src = 'https://neto-api.herokuapp.com/twitter/jsonp';
+document.querySelector('body').appendChild(scriptNode);
