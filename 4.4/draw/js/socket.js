03744ca4bdcb71df399d6ec3ab17d778a5e622a7
Vladislav Semenov <affliction001@mail.ru>
2018-08-03 12:30:29 +0300

    For checking

--- 4.4/draw/js/socket.js
+++ 4.4/draw/js/socket.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+ws.addEventListener('open', e => {
+  editor.addEventListener('update', e => {
+    e.canvas.toBlob(img => {
+      ws.send(img);
+    });
+  });
+});
