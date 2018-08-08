c89c68efa97c532e72a4cd24ba79c61f4b18735b
Vladislav Semenov <affliction001@mail.ru>
2018-08-08 19:03:26 +0300

    Всевидящее око Бендера следит за тобой

--- 5.1/eye/js/animation.js
+++ 5.1/eye/js/animation.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const pupil = document.querySelector('.big-book__pupil');
+const pupilCoords = pupil.getBoundingClientRect();
+const pupilCenterX = pupilCoords.left + pupilCoords.width / 2;
+const pupilCenterY = pupilCoords.top + pupilCoords.height / 2;
+
+document.addEventListener('mousemove', e => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  pupil.style.setProperty('--pupil-x', 30 * (mouseX * 100 / pupilCenterX) / 100 - 30 + 'px');
+  pupil.style.setProperty('--pupil-y', 30 * (mouseY * 100 / pupilCenterY) / 100 - 30 + 'px');
+  pupil.style.setProperty('--pupil-size', getSize(mouseX, mouseY, pupilCenterX, pupilCenterY));
+});
+
+function getSize(mX, mY, pX, pY) {
+  const msX = Math.abs(pX - mX) * 100 / pX;
+  const msY = Math.abs(pY - mY) * 100 / pY;
+
+  if (msX >= 66.66 || msY >= 66.66) {
+    return 1;
+  } else if (msX >= 33.33 || msY >= 33.33) {
+    return 2;
+  }
+
+  return 3;
+}
