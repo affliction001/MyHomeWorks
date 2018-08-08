3c92d4b14ffa4edd736d766f1fdf96dfb43edcdb
Vladislav Semenov <affliction001@mail.ru>
2018-08-08 20:16:16 +0300

    Глаз работает

--- 5.1/eye/js/animation.js
+++ 5.1/eye/js/animation.js
@@ -1,28 +1,58 @@
 'use strict';
 
 const pupil = document.querySelector('.big-book__pupil');
-const pupilCoords = pupil.getBoundingClientRect();
-const pupilCenterX = pupilCoords.left + pupilCoords.width / 2;
-const pupilCenterY = pupilCoords.top + pupilCoords.height / 2;
+
 
 document.addEventListener('mousemove', e => {
   const mouseX = e.clientX;
   const mouseY = e.clientY;
 
-  pupil.style.setProperty('--pupil-x', 30 * (mouseX * 100 / pupilCenterX) / 100 - 30 + 'px');
-  pupil.style.setProperty('--pupil-y', 30 * (mouseY * 100 / pupilCenterY) / 100 - 30 + 'px');
-  pupil.style.setProperty('--pupil-size', getSize(mouseX, mouseY, pupilCenterX, pupilCenterY));
+  updateEye(mouseX, mouseY);
 });
 
-function getSize(mX, mY, pX, pY) {
-  const msX = Math.abs(pX - mX) * 100 / pX;
-  const msY = Math.abs(pY - mY) * 100 / pY;
+document.addEventListener('scroll', e => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
 
-  if (msX >= 66.66 || msY >= 66.66) {
-    return 1;
-  } else if (msX >= 33.33 || msY >= 33.33) {
-    return 2;
+  updateEye(mouseX, mouseY);
+});
+
+function updateEye(mouseX, mouseY) {
+  const pupilCoords = pupil.getBoundingClientRect();
+  const pupilCenterX = pupilCoords.left + pupilCoords.width / 2;
+  const pupilCenterY = pupilCoords.top + pupilCoords.height / 2;
+
+  pupil.style.setProperty('--pupil-x', getX(mouseX) + 'px');
+  pupil.style.setProperty('--pupil-y', getY(mouseY) + 'px');
+  pupil.style.setProperty('--pupil-size', getSize(mouseX, mouseY));
+
+  function getX(mX) {
+    return mX <= pupilCenterX ?
+      -((pupilCenterX - mX) * 100 / pupilCenterX) * 30 / 100 :
+      (mX - pupilCenterX) * 100 / (window.innerWidth - pupilCenterX) * 30 / 100;
   }
 
-  return 3;
+  function getY(mY) {
+    return mY <= pupilCenterY ?
+      -((pupilCenterY - mY) * 100 / pupilCenterY) * 30 / 100 :
+      (mY - pupilCenterY) * 100 / (window.innerHeight - pupilCenterY) * 30 / 100;
+  }
+
+  function getSize(mX, mY) {
+    const msX = mX <= pupilCenterX ?
+      ((pupilCenterX - mX) * 100 / pupilCenterX) :
+      (mX - pupilCenterX) * 100 / (window.innerWidth - pupilCenterX);
+
+    const msY = mY <= pupilCenterY ?
+      ((pupilCenterY - mY) * 100 / pupilCenterY) :
+      (mY - pupilCenterY) * 100 / (window.innerHeight - pupilCenterY);
+
+    if (msX >= 66.66 || msY >= 66.66) {
+      return 1;
+    } else if (msX >= 33.33 || msY >= 33.33) {
+      return 2;
+    }
+
+    return 3;
+  }
 }
